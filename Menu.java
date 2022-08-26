8adceadd725288a17778f29a85e08505601601c6
Benjaaaa <benjamed.loco@gmail.com>
2022-08-26 19:24:54 -0400

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.util.LinkedList;
+
+import java.io.IOException;
 
 public class Menu {
     static private BufferedReader br;
