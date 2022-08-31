28ba620b8acc6c6eb762bcf6ece7a1a59a6efafa
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-08-31 18:05:55 -0400

    comentar super de clases sin terminar

--- src/Debate.java
+++ src/Debate.java
@@ -6,7 +6,7 @@ public class Debate extends Presentacion {
     public Debate(Persona expositor, int duracion, int asistentesCantidad, 
     			  String tema, String descripcion, String fecha,int cantAsistentesOpuestos, 
     			  String expositorOpuesto, Persona [] asistants) {
-    	super (expositor, duracion, asistentesCantidad, tema, descripcion, fecha, asistants);
+    	// super (expositor, duracion, asistentesCantidad, tema, descripcion, fecha, asistants);
     	this.expositorOpuesto = expositorOpuesto;
     	this.cantAsistente = cantAsistentesOpuestos;
     	this.asistentesOpuestos = new Persona[cantAsistentesOpuestos];
--- src/Votacion.java
+++ src/Votacion.java
@@ -5,7 +5,7 @@ public class Votacion extends Presentacion {
     public Votacion(Persona expositor, int duracion, int asistentesCantidad, 
 					String tema, String descripcion, String fecha, 
 					int cantidadOpciones, Persona[] assistants){
-    	super (expositor, duracion, asistentesCantidad, tema, descripcion, fecha, assistants);
+    	// super (expositor, duracion, asistentesCantidad, tema, descripcion, fecha, assistants);
     	this.cantidadOpciones = cantidadOpciones;
 		this.opciones = new String[cantidadOpciones];
     }
