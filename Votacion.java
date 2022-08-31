6e05f028116f7bc220889bbda676b908bd722f02
Your Name <you@example.com>
2022-08-30 22:13:20 -0400

    añadir hijos de presentacion

---
+++ Votacion.java
@@ -0,0 +1,12 @@
+public class Votacion extends Presentacion {
+	private int cantidadOpciones;
+    private String[] opciones;
+    
+    public Votacion(Persona expositor, int duracion, int asistentesCantidad, 
+					String tema, String descripcion, String fecha, 
+					int cantidadOpciones, Persona[] assistants){
+    	super (expositor, duracion, asistentesCantidad, tema, descripcion, fecha, assistants);
+    	this.cantidadOpciones = cantidadOpciones;
+		this.opciones = new String[cantidadOpciones];
+    }
+}
