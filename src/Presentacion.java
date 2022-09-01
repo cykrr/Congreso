7ed08e1068d977444c022a1ae950f4e0a9c03f5d
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-09-01 01:19:17 -0400

    añadir setters para presentacion

--- src/Presentacion.java
+++ src/Presentacion.java
@@ -1,3 +1,7 @@
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.util.LinkedList;
+
 public class Presentacion {
 	private String nombre;
 	private String fecha;
@@ -5,6 +9,9 @@ public class Presentacion {
 	private int duracion;
 	private String descripcion;
 	private Persona expositor;
+    private LocalDate localDate = LocalDate.of(1993,01, 01);
+    private LocalTime localTime = LocalTime.of(0, 0);
+    private LinkedList<Persona> asistentes;
     
     public Presentacion(String nombre) {
     	this.nombre = nombre;
@@ -17,13 +24,37 @@ public class Presentacion {
     public void setFecha(String fecha) {
     	this.fecha = fecha;
     }
+
+    public void setFecha(int dia, int mes, int ano) {
+        this.localDate = LocalDate.of(ano, mes, dia);
+    }
     
     public void setHora(String hora) {
     	this.hora = hora;
     }
+
+    public void setHora(int hora) {
+        localTime = LocalTime.of(hora, 0);
+    }
+
+    public void setMinuto(int min) {
+        localTime = LocalTime.of(localTime.getHour(), min);
+    }
     
     public void setDuracion(int duracion) {
     	this.duracion = duracion;
+    }
+
+    public void setDia(int dia) {
+        this.localDate = LocalDate.of(localDate.getYear(), localDate.getMonth(), dia);
+    }
+
+    public void setMes(int mes) {
+        this.localDate = LocalDate.of(localDate.getYear(), mes, localDate.getDayOfMonth());
+    }
+
+    public void setAno(int ano) {
+        this.localDate = LocalDate.of(ano, localDate.getMonth(), localDate.getDayOfMonth());
     }
     
     public void setExpositor(Persona expositor) {
@@ -34,10 +65,21 @@ public class Presentacion {
     	return nombre;
     }
 
-	public int getId() {
-		return this.ID;
-	}
+	// public int getId() {
+		// return this.ID;
+	// }
     
+    public void setAsistentes(String asistentes) {
+        if (asistentes.charAt(0) != '\"') {
+            System.err.println("Error: setAsistentes() Esperado string" +
+                    "que inicia con '\"'.");
+            System.exit(1);
+            asistentes = asistentes.substring(1, asistentes.length() - 1);
+            this.asistentes = CSVTokener.csvArray(new CSVTokener(asistentes));
+        }
+
+    }
+
     public void mostrar() {
     	System.out.println("Nombre: " + nombre);
     	System.out.println("Expositor: " + (expositor != null ? expositor.getNombre() : "No asignado"));
