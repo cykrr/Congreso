bb7ed8b648168936fb0d826a92567083cb3c5b56
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-09-13 19:17:35 -0300

    terminar gui

--- GUI/src/main/java/Gui/Gui.java
+++ GUI/src/main/java/Gui/Gui.java
@@ -1,16 +1,44 @@
 package Gui;
+
+import java.io.IOException;
+
 import Congreso.Registro;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 public class Gui extends Application {
 
-    Stage stage;
+    private     Stage       stage;
+    private     VBox        root;
+    private     Registro    r;
     
     @Override
     public void start(Stage s) {
-        Registro r;
         System.out.println("Congreso GUI");
         this.stage = s;
+        inflar();
+    }
+
+    private void inflar() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Gui.class.getResource("/vistas/root.fxml"));
+        System.out.println((Gui.class.getResource("/vistas/root.fxml")));
+        try {
+            root = (VBox)loader.load();
+            Scene s = new Scene(root);
+            stage.setScene(s);
+            stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.err.println("Error: No se encontró el recurso");
+            System.exit(1);
+        }
+
     }
 }
