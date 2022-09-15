100390916efc9d30510c921fa1ab8f1554d74f99
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-09-15 11:09:46 -0300

    feat: crear ListaPresentaciones
    s

---
+++ GUI/src/main/java/Gui/Vistas/ListaPresentaciones/ListaPresentaciones.java
@@ -0,0 +1,15 @@
+package Gui.Vistas.ListaPresentaciones;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+public class ListaPresentaciones extends VBox {
+    public ListaPresentaciones() {
+
+    }
+
+
+}
