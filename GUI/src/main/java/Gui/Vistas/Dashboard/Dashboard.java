12641370d13ac5c4cc89bda600ed9cfbe5b9e3c4
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-09-18 04:18:37 -0300

    feat: habilitar clickeo botones

--- GUI/src/main/java/Gui/Vistas/Dashboard/Dashboard.java
+++ GUI/src/main/java/Gui/Vistas/Dashboard/Dashboard.java
@@ -4,25 +4,47 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Congreso.Presentacion;
 import Congreso.Registro;
-
+import Gui.EventoPresentacion;
 // TODO : Sacar de paquete
 import Gui.Vistas.Busqueda.Busqueda;
-
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 public class Dashboard extends VBox implements Initializable {
+
+
+
     private     Registro    registro;
 
     @FXML
     private     Busqueda    cajaBusqueda;
 
     @FXML
+    private     ScrollPane        scroll;
+
+    @FXML
     private     VBox        scrollBox;
+
+    @FXML 
+    private     VBox        botonPresentaciones;
+    @FXML
+    private     VBox        botonExpositores;
+    @FXML
+    private     VBox        botonAsistentes;
+
+    VBox seleccion;
+
+    @FXML void click(MouseEvent me) {
+        System.out.println("beep");
+    }
 
     public Dashboard(Registro r) {
         super();
@@ -41,7 +63,37 @@ public class Dashboard extends VBox implements Initializable {
     }
     public void initialize(URL url, ResourceBundle rb)
     {
-
+        seleccion = botonPresentaciones;
+        seleccion.getStyleClass().add("selected");
+        scroll.setFitToWidth(true);
+        scroll.setFitToHeight(true);
+        botonPresentaciones.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                if (seleccion != botonPresentaciones) {
+                    seleccion.getStyleClass().remove("selected");
+                    seleccion = botonPresentaciones;
+                    seleccion.getStyleClass().add("selected");
+                }
+            }
+        });
+        botonExpositores.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                if (seleccion != botonExpositores) {
+                    seleccion.getStyleClass().remove("selected");
+                    seleccion = botonExpositores;
+                    seleccion.getStyleClass().add("selected");
+                }
+            }
+        });
+        botonAsistentes.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent event) {
+                if (seleccion != botonAsistentes) {
+                    seleccion.getStyleClass().remove("selected");
+                    seleccion = botonAsistentes;
+                    seleccion.getStyleClass().add("selected");
+                }
+            }
+        });
     }
     
     public void buscar() {
