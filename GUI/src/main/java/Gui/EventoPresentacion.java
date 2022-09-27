23b06fa4b883ac7456ad951dbe69fc9d603436e5
benjamín robles <benjamin.robles.a@mail.pucv.cl>
2022-09-26 22:09:45 -0300

    Dar formato

--- GUI/src/main/java/Gui/EventoPresentacion.java
+++ GUI/src/main/java/Gui/EventoPresentacion.java
@@ -5,16 +5,20 @@ import javafx.event.Event;
 import javafx.event.EventType;
 
 public class EventoPresentacion extends Event {
-    public static final EventType<EventoPresentacion> ANY = new EventType<>(Event.ANY, "ANY");
+    public static final EventType<EventoPresentacion> ANY = new EventType<>(
+            Event.ANY, "ANY");
 
-    public static final EventType<EventoPresentacion> CREAR_PRESENTACION = 
-        new EventType<>(EventoPresentacion.ANY, "CREAR_PRESENTACION");
+    public static final EventType<EventoPresentacion> CREAR_PRESENTACION = new EventType<>(
+            EventoPresentacion.ANY,
+            "CREAR_PRESENTACION");
 
-    public static final EventType<EventoPresentacion> EDITAR_PRESENTACION =
-        new EventType<>(EventoPresentacion.ANY, "EDITAR_PRESENTACION");
+    public static final EventType<EventoPresentacion> EDITAR_PRESENTACION = new EventType<>(
+            EventoPresentacion.ANY,
+            "EDITAR_PRESENTACION");
 
-    public static final EventType<EventoPresentacion> ELIMINAR_PRESENTACION =
-        new EventType<>(EventoPresentacion.ANY, "ELIMINAR_PRESENTACION");
+    public static final EventType<EventoPresentacion> ELIMINAR_PRESENTACION = new EventType<>(
+            EventoPresentacion.ANY,
+            "ELIMINAR_PRESENTACION");
 
     private Presentacion p1;
     private Presentacion p2;
@@ -23,22 +27,23 @@ public class EventoPresentacion extends Event {
         super(et);
         this.p1 = p;
     }
-    
-    public EventoPresentacion(EventType<? extends Event> et, Presentacion p1, Presentacion p2) {
-    	super(et);
-    	this.p1 = p1;
-    	this.p2 = p2;
+
+    public EventoPresentacion(EventType<? extends Event> et, Presentacion p1,
+            Presentacion p2) {
+        super(et);
+        this.p1 = p1;
+        this.p2 = p2;
     }
 
     public Presentacion getPresentacion() {
         return p1;
     }
-    
+
     public Presentacion getPresentacionAntigua() {
-    	return p1;
+        return p1;
     }
-    
+
     public Presentacion getPresentacionNueva() {
-    	return p2;
+        return p2;
     }
 }
